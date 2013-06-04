09d285b71f6446846a9b758cbf90b7982e8a6c93
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-06-04 13:31:44 +0200

    better implementation of title property for page

--- config/routes.rb
+++ config/routes.rb
@@ -9,25 +9,23 @@ Wiki::Application.routes.draw do
     get '/:id' => 'contributions#show', :as => :contribution
   end
 
-  authenticated :user do
-    root :to => 'home#index'
-  end
-
   root :to => "home#index"
   get '/login_intro' => 'home#login_intro'
-  get '/wiki' => 'pages#show'
   get '/tours' => 'tours#index'
   get '/search' => 'pages#search'
-  match '/wiki/:title' => 'pages#show' , :constraints => { :id => /.*/ }
-  match '/tours/:title' => 'tours#show'
+  match '/tours/:id' => 'tours#show'
 
   #users
   match 'registrations' => 'users#index', :as => 'registrations'
   devise_for :users, :controllers => { :registrations => 'registrations' }
   resources :users, :only => [:show, :index]
 
+  # pages routes
+  get '/wiki' => 'pages#show'
+  match '/wiki/:id' => 'pages#show' , :constraints => { :id => /.*/ }
+
+  # json api requests for pages
   scope 'api', :format => :json do
-    post 'classify' => 'classification#classify'
     post 'parse' => 'pages#parse'
     get 'pages' => 'pages#all'
     resources :pages, :constraints => { :id => /.*/ }, :only => [:section,:show] do
@@ -37,7 +35,7 @@ Wiki::Application.routes.draw do
         delete '/' => 'pages#delete'
         get 'sections' => 'pages#sections'
         get 'internal_links' => 'pages#internal_links'
-        get 'sections/:title' => 'pages#section'
+        get 'sections/:id' => 'pages#section'
         get 'summary' => 'pages#summary'
       end
     end
@@ -46,7 +44,7 @@ Wiki::Application.routes.draw do
   scope 'endpoint', :format => :json do
     get ':id/rdf' => 'pages#get_rdf', :constraints => { :id => /.*/ }
     get ':id/json' => 'pages#get_json', :constraints => { :id => /.*/ }
-  end  
+  end
 
   devise_for :users, :controllers => { :registrations => 'registrations' }
   resources :users, :only => [:show,:destroy]
